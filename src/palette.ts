a33ffd5166f1aafae2be967619df5069d3284900
Blindeen <kuba.szmajda@gmail.com>
2024-04-07 21:01:59 +0200

    create navbar component

--- src/palette.ts
+++ src/palette.ts
@@ -6,6 +6,9 @@ const palette = {
     darkCharcoal: '#333333',
     blue: '#154ABD',
     steam: '#DDDDDD',
+    darkNostalgiaSkyBlue: '#1A8AD3',
+    forrestLagoon: '#48B1EA',
+    paleMorningBlueSky: '#D5EDFA',
 };
 
 export default palette;
