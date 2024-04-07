847eb6103ec05af012f8a4f468719689540b21a2
Blindeen <kuba.szmajda@gmail.com>
2024-04-07 10:07:57 +0200

    add palette with common colors

---
+++ src/palette.ts
@@ -0,0 +1,11 @@
+const palette = {
+    red: '#FF0000',
+    spinningSnowflake: '#F1F3F1',
+    white: '#FFFFFF',
+    black: '#000000',
+    darkCharcoal: '#333333',
+    blue: '#154ABD',
+    steam: '#DDDDDD',
+};
+
+export default palette;
