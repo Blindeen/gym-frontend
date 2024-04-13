45c8bbd2065400516a0fc1e764788e24d0e125b6
Blindeen <kuba.szmajda@gmail.com>
2024-04-13 11:30:13 +0200

    move color palette to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,18 @@ export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {},
+        colors: {
+            red: '#FF0000',
+            spinningSnowflake: '#F1F3F1',
+            white: '#FFFFFF',
+            black: '#000000',
+            darkCharcoal: '#333333',
+            blue: '#154ABD',
+            steam: '#DDDDDD',
+            darkNostalgiaSkyBlue: '#1A8AD3',
+            forrestLagoon: '#48B1EA',
+            paleMorningBlueSky: '#D5EDFA',
+        },
     },
     plugins: [],
 } satisfies Config;
