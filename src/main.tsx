bca4602600ff2832b6851c2f1fceb1c55ea5ac5b
Blindeen <kuba.szmajda@gmail.com>
2024-04-13 16:30:08 +0200

    set up router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css';
+import LandingPage from './pages/LandingPage';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <LandingPage />,
+    },
+    {
+        path: '/login',
+        element: null,
+    },
+    {
+        path: '/register',
+        element: null,
+    },
+    {
+        path: '/activities',
+        element: null,
+    },
+    {
+        path: '/trainer-dashboard',
+        element: null,
+    },
+    {
+        path: '/client-dashboard',
+        element: null,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
