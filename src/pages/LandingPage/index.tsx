5b3a46b5dd32e72b6371816ae6ebfbb4cf3bb7e9
Blindeen <kuba.szmajda@gmail.com>
2024-04-13 13:14:05 +0200

    rewrite first section

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,61 +1,39 @@
-import { Flex, Image } from '@chakra-ui/react';
-
 import Navbar from '../../components/Navbar';
-import palette from '../../palette.ts';
 
 const LandingPage = () => {
+    const linkItems = [
+        'Home',
+        'Activities',
+        'Trainers',
+        'Contact',
+        'Sign up',
+        'Sign in',
+    ];
+
     return (
         <>
-            <Flex
-                direction="column"
-                gap="40px"
-                pb={75}
-                borderBottomRadius="30px"
-                bgGradient={`linear(to-t, ${palette.blue} 0.1%, ${palette.darkNostalgiaSkyBlue} 51.5%, ${palette.forrestLagoon} 100.2%)`}
-            >
-                <Navbar />
-                <Flex
-                    alignItems="center"
-                    width="75%"
-                    height="400px"
-                    alignSelf="center"
-                    borderRadius="30px"
-                    bgColor={palette.white}
-                >
-                    <Flex
-                        direction="column"
-                        justifyContent="center"
-                        paddingX="40px"
-                        borderRadius="30px"
-                        maxWidth="35%"
-                        height="inherit"
-                        bgColor={palette.paleMorningBlueSky}
-                        boxShadow="10px 0 10px -5px rgba(0, 0, 0, 0.5)"
-                    >
-                        <h2>Fitsphere</h2>
-                        <h4>Get Fit, Stay Fit</h4>
-                        <p style={{ textAlign: 'justify' }}>
+            <div className="flex flex-col items-center gap-[40px] rounded-b-large pb-[75px] bg-gradient-to-t from-blue from-0.1% via-darkNostalgiaSkyBlue via-51.5% to-forrestLagoon to-100.2%">
+                <Navbar items={linkItems} />
+                <div className="flex items-center w-[75%] h-[400px] rounded-large bg-white">
+                    <div className="flex flex-col justify-center max-w-[35%] h-full rounded-large px-[40px] bg-paleMorningBlueSky shadow-slight sm:max-w-[100%] lg:max-w-[35%]">
+                        <h2 className="h2">FitSphere</h2>
+                        <h4 className="h4">Get Fit, Stay Fit</h4>
+                        <p className="text-justify mt-[10px]">
                             We're more than just a place to break a sweat. We're
                             more than just a place to break a sweat. We're your
                             fitness sanctuary and your second home, whether
                             you're an athlete or just a beginner. We've got
                             something for everyone.
                         </p>
-                    </Flex>
-                    <Flex
-                        justifyContent="center"
-                        alignItems="end"
-                        flexGrow={1}
-                        height="inherit"
-                    >
-                        <Image
-                            src="src/assets/img/white-woman-workout.png"
+                    </div>
+                    <div className="flex flex-1 h-full justify-center items-end sm:hidden lg:flex">
+                        <img
+                            src="/src/assets/img/white-woman-workout.png"
                             alt="fitness-class-banner"
-                            filter="drop-shadow(5px 0 5px rgba(0,0,0,0.4))"
                         />
-                    </Flex>
-                </Flex>
-            </Flex>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
