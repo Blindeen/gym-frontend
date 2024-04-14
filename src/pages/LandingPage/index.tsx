b0e1cf95b0597a9ccbb22e9ea29838bf71d20541
Blindeen <kuba.szmajda@gmail.com>
2024-04-14 14:57:55 +0200

    add second section on landing page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,6 +1,45 @@
 import Navbar from '../../components/Navbar';
 
+interface ServiceItem {
+    header: string;
+    imgSrc: string;
+    altText: string;
+    description: string;
+}
+
+const serviceItems: ServiceItem[] = [
+    {
+        header: 'Personal training',
+        imgSrc: '/src/assets/img/personal-training.png',
+        altText: 'personal-training',
+        description: 'One-on-one training with our experienced trainers.',
+    },
+    {
+        header: 'Group Classes',
+        imgSrc: '/src/assets/img/group-classes.png',
+        altText: 'gym-classes',
+        description: 'Join our group fitness classes for a fun workout.',
+    },
+    {
+        header: 'Nutrition Guidance',
+        imgSrc: '/src/assets/img/protein-shake.png',
+        altText: 'protein-shake',
+        description: 'Get expert advice on your diet and nutrition.',
+    },
+];
+
 const LandingPage = () => {
+    const serviceElements = serviceItems.map((item, idx) => (
+        <div
+            key={idx}
+            className="flex flex-col justify-center items-center rounded-large bg-white shadow-[0_0_10px_0_rgba(0,0,0,0.5)] sm:p-0 sm:w-[80%] lg:p-[20px] lg:w-auto"
+        >
+            <h4 className="h4 mb-4">{item.header}</h4>
+            <img src={item.imgSrc} alt={item.altText} />
+            <p className="text-justify p-2.5">{item.description}</p>
+        </div>
+    ));
+
     return (
         <>
             <div className="flex flex-col items-center gap-[40px] rounded-b-large pb-[75px] bg-gradient-to-t from-blue from-0.1% via-darkNostalgiaSkyBlue via-51.5% to-forrestLagoon to-100.2%">
@@ -24,6 +63,10 @@ const LandingPage = () => {
                         />
                     </div>
                 </div>
+            </div>
+            <h2 className="h2 text-center my-3">Our services</h2>
+            <div className="flex justify-evenly flex-wrap my-[40px] sm:gap-y-5 lg:gap-0">
+                {serviceElements}
             </div>
         </>
     );
