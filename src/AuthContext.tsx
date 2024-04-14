bf0093d8852a4e5b30da7bdb4dfe8dee13674ea7
Blindeen <kuba.szmajda@gmail.com>
2024-04-14 20:21:36 +0200

    extend context

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -1,32 +1,44 @@
 import { createContext, FC, ReactNode, useState } from 'react';
 
+export type Role = 'GUEST' | 'CLIENT' | 'TRAINER';
+
 interface User {
     id: number | null;
     email: string;
     token: string;
 }
 
+interface State {
+    isLogged: boolean;
+    role: Role;
+    user: User;
+}
+
 type AuthContextType = {
-    userInfo: User;
-    setUserInfo: (userInfo: User) => void;
+    state: State;
+    setState: (state: State) => void;
 };
 
-const defaultUserValue: User = {
-    id: null,
-    email: '',
-    token: '',
+const defaultStateValue: State = {
+    isLogged: false,
+    role: 'GUEST',
+    user: {
+        id: null,
+        email: '',
+        token: '',
+    },
 };
 
 export const AuthContext = createContext<AuthContextType>({
-    userInfo: defaultUserValue,
-    setUserInfo: () => {},
+    state: defaultStateValue,
+    setState: () => {},
 });
 
 const AuthSessionProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const [userInfo, setUserInfo] = useState(defaultUserValue);
+    const [state, setState] = useState(defaultStateValue);
 
     return (
-        <AuthContext.Provider value={{ userInfo, setUserInfo }}>
+        <AuthContext.Provider value={{ state, setState }}>
             {children}
         </AuthContext.Provider>
     );
