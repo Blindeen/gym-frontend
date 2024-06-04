c8063ab1ba0973b6e7acb0491d6bc7a8b8750117
Blindeen <kuba.szmajda@gmail.com>
2024-06-04 19:00:48 +0200

    extend button interface

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,30 @@
+import { HTMLAttributes } from 'react';
+
 enum ButtonType {
     primary = 'bg-blue-500 text-white',
     secondary = 'bg-gray-400 text-white',
     danger = 'bg-red-500 text-white',
 }
 
-interface ButtonProps {
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
     children: string;
-    type?: keyof typeof ButtonType;
+    buttonType?: keyof typeof ButtonType;
     onClick?: () => void;
 }
 
-const Button = ({ children, type = 'primary', onClick }: ButtonProps) => {
-    const buttonType = ButtonType[type];
+const Button = ({
+    children,
+    buttonType = 'primary',
+    onClick,
+    ...props
+}: ButtonProps) => {
+    const buttonStyle = ButtonType[buttonType];
 
     return (
         <button
-            className={`min-w-28 p-2 rounded-large ${buttonType} font-bold`}
-            type="button"
+            className={`min-w-28 p-2 rounded-large ${buttonStyle} font-bold`}
             onClick={onClick}
+            {...props}
         >
             {children}
         </button>
