670aa434323f7734ef69e57c78d735ab89dac538
Blindeen <kuba.szmajda@gmail.com>
2024-06-04 21:09:04 +0200

    refactor register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,7 +1,17 @@
-import Button from '@/components/Button';
+import RegisterForm from '@/containers/RegisterForm';
+
+import PageBackground from '@/components/PageBackground';
+import FormCard from '@/components/form/FormCard';
 
 const RegisterPage = () => {
-    return <Button type="danger">elo</Button>;
+    return (
+        <PageBackground>
+            <FormCard>
+                <h2 className="h2-primary">Sign up</h2>
+                <RegisterForm />
+            </FormCard>
+        </PageBackground>
+    );
 };
 
 export default RegisterPage;
