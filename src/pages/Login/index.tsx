eeba8569ac472646fb8e2ee2a22097adc50be3a6
Blindeen <kuba.szmajda@gmail.com>
2024-06-05 16:41:53 +0200

    create login page component

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import FormCard from '@/components/form/FormCard';
+
+const LoginPage = () => {
+    return (
+        <FormCard>
+            <h2 className="h2-primary mt-0">Sign in</h2>
+        </FormCard>
+    );
+};
+
+export default LoginPage;
