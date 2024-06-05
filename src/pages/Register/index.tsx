7dfa2c60da982cd85d61439087fe1eb04fd8b8e0
Blindeen <kuba.szmajda@gmail.com>
2024-06-05 20:46:56 +0200

    fix temporary navbar

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -5,13 +5,13 @@ import Navbar from '@/components/Navbar';
 
 const RegisterPage = () => {
     return (
-        <>
+        <div className="flex flex-col h-full">
             <Navbar bgColor="blue-500" />
             <FormCard>
                 <h2 className="h2-primary mt-0">Sign up</h2>
                 <RegisterForm />
             </FormCard>
-        </>
+        </div>
     );
 };
 
