ba4ede16427d7b182fd8264ab0b7a0011b9a9736
Blindeen <kuba.szmajda@gmail.com>
2024-06-06 19:17:42 +0200

    add leave activity func

--- src/containers/tables/CustomerTable/index.tsx
+++ src/containers/tables/CustomerTable/index.tsx
@@ -1,48 +1,17 @@
 import { useContext } from 'react';
 
+import { toast } from 'react-toastify';
+
 import Button from '@/components/Button';
 import Table from '@/components/Table';
 
 import { AuthContext } from '@/AuthContext.tsx';
-import { ActivitiesResponse, Column } from '@/interfaces.ts';
-
-const columns: Column[] = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Day of week',
-        dataIndex: 'dayOfWeek',
-        key: 'dayOfWeek',
-    },
-    {
-        title: 'Start time',
-        dataIndex: 'startTime',
-        key: 'startTime',
-    },
-    {
-        title: 'End time',
-        dataIndex: 'endTime',
-        key: 'endTime',
-    },
-    {
-        title: 'Room',
-        dataIndex: 'room',
-        key: 'room',
-    },
-    {
-        title: 'Action',
-        dataIndex: 'action',
-        key: 'action',
-        render: () => (
-            <div className="flex gap-2">
-                <Button size={20}>Unsubscribe</Button>
-            </div>
-        ),
-    },
-];
+import {
+    ActivitiesResponse,
+    Column,
+    type ErrorResponse,
+} from '@/interfaces.ts';
+import axios from '@/api.ts';
 
 interface CustomerTableProps {
     data: ActivitiesResponse;
@@ -51,6 +20,75 @@ interface CustomerTableProps {
 
 const CustomerTable = ({ data, fetchActivities }: CustomerTableProps) => {
     const { state } = useContext(AuthContext);
+
+    const leaveActivity = (id: number) => {
+        const res = axios.delete(`/activity/${id}/leave`, {
+            headers: {
+                Authorization: `Bearer ${state.token}`,
+            },
+        });
+
+        res.then(() => {
+            toast('You have left the activity', {
+                type: 'success',
+            });
+            fetchActivities();
+        }).catch((err) => {
+            if (err.response) {
+                const { error } = err.response.data as ErrorResponse;
+                toast(error, {
+                    type: 'error',
+                });
+            } else {
+                toast('An error occurred', {
+                    type: 'error',
+                });
+            }
+        });
+    };
+
+    const columns: Column[] = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Day of week',
+            dataIndex: 'dayOfWeek',
+            key: 'dayOfWeek',
+        },
+        {
+            title: 'Start time',
+            dataIndex: 'startTime',
+            key: 'startTime',
+        },
+        {
+            title: 'End time',
+            dataIndex: 'endTime',
+            key: 'endTime',
+        },
+        {
+            title: 'Room',
+            dataIndex: 'room',
+            key: 'room',
+        },
+        {
+            title: 'Action',
+            dataIndex: 'action',
+            key: 'action',
+            render: (record) => (
+                <div className="flex gap-2">
+                    <Button
+                        size={20}
+                        onClick={() => leaveActivity(record['id'])}
+                    >
+                        Unsubscribe
+                    </Button>
+                </div>
+            ),
+        },
+    ];
 
     return (
         <div className="sm:w-[90%] md:w-[70%] lg:w-[60%]">
