358468e1d606e19e302c7968d3fae26e679b3fee
Blindeen <kuba.szmajda@gmail.com>
2024-06-12 23:42:13 +0200

    adjust eslint config to the newest version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,17 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    files: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
+    languageOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    ignores: ['dist', '.eslintrc.cjs'],
+    plugins: {
+        'react-refresh': require('eslint-plugin-react-refresh'),
+    },
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+    },
+};
