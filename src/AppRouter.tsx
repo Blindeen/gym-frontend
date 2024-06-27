115683b4b3dae581727bc8cf3db5356da8d249d3
Blindeen <kuba.szmajda@gmail.com>
2024-06-27 14:07:51 +0200

    move router to a higher order component

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Layout from '@/components/Layout';
 import LandingPage from '@/pages/LandingPage';
@@ -7,13 +7,11 @@ import routes from './routes.ts';
 
 const AppRouter = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path={routes.home} element={<Layout />}>
-                    <Route index element={<LandingPage />} />
-                </Route>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path={routes.home} element={<Layout />}>
+                <Route index element={<LandingPage />} />
+            </Route>
+        </Routes>
     );
 };
 
