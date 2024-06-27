24c1944bf4591a84bfe5217fad2e7d4692ea8f0f
Blindeen <kuba.szmajda@gmail.com>
2024-06-27 15:25:32 +0200

    add additional routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 const routes = {
     home: '/',
+    about: '/about',
     login: '/login',
     register: '/register',
     activities: '/activities',
     dashboard: '/dashboard',
+    stuff: '/stuff',
+    contact: '/contact',
 };
 
 export default routes;
