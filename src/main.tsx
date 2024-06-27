82eeb0e72c8fd324b634613a3a6ad8ad86ce7762
Blindeen <kuba.szmajda@gmail.com>
2024-06-27 14:03:21 +0200

    create separate app component

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { NextUIProvider } from '@nextui-org/react';
+import App from '@/App.tsx';
 
 import '@/index.css';
-import AuthSessionProvider from '@/AuthContext.tsx';
-import AppRouter from '@/AppRouter.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <AuthSessionProvider>
-            <NextUIProvider>
-                <AppRouter />
-            </NextUIProvider>
-        </AuthSessionProvider>
+        <App />
     </React.StrictMode>
 );
