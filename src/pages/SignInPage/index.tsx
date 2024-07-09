329594e6e21defeb1e7efbff7d6f1973492e667c
Blindeen <kuba.szmajda@gmail.com>
2024-07-09 20:55:38 +0200

    create sign-in page component

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -1,12 +1,15 @@
-import LoginImage from '/images/illustrations/login.svg';
 import { Image } from '@nextui-org/react';
+import LoginImage from '/images/illustrations/login.svg';
+
+import Logo from '@/components/Logo';
+import SignInForm from '@/components/SignInForm';
 
 const SignInPage = () => {
     return (
-        <div className="flex flex-wrap gap-y-5 lg:min-h-screen">
+        <div className="min-h-screen flex flex-col justify-center gap-y-8 md:flex-row md:gap-y-0">
             <div className="w-full flex justify-center md:w-1/2 md:bg-secondary md:h-screen md:items-center">
                 <Image
-                    className="w-2/3 mt-5 md:mt-0"
+                    className="w-2/3"
                     src={LoginImage}
                     alt="Sign in image"
                     radius="none"
@@ -14,7 +17,10 @@ const SignInPage = () => {
                     draggable={false}
                 />
             </div>
-            <div className="w-full md:w-1/2"></div>
+            <div className="w-full flex flex-col items-center gap-y-4 md:w-1/2 md:justify-center">
+                <Logo clickable={false} />
+                <SignInForm />
+            </div>
         </div>
     );
 };
