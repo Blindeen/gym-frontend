0ee352705ea4e48d05ec02e424cd7365c3587fd5
Blindeen <kuba.szmajda@gmail.com>
2024-07-13 21:04:25 +0200

    create separate colors file

---
+++ src/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+    violentViolet: '#2E1067',
+    violet: '#1F094A',
+    oldBlueEyesIrises: '#526FFF',
+    pictonBlue: '#41C9E2',
+    white: '#FFFFFF',
+    twitterDim: '#15202B',
+};
+
+export default colors;
