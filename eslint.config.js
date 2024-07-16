910c93d148c346d91044074d34764fcac069b9bd
Blindeen <kuba.szmajda@gmail.com>
2024-07-16 20:44:02 +0200

    add globals to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
 import js from '@eslint/js';
 import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 export default {
     name: 'ESLint config',
     files: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
-    ignores: ['**/*.config.*', '**/*.cjs'],
+    ignores: ['**/*.config.*'],
     languageOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
         parser: tsParser,
+        globals: {
+            ...globals.browser,,
+        },
     },
     rules: {
         ...js.configs.recommended.rules,
