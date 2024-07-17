a65b6b9539468ca7d04654eb761dc16cad7a1389
Blindeen <kuba.szmajda@gmail.com>
2024-07-17 19:52:55 +0200

    create pagination types

--- src/types.ts
+++ src/types.ts
@@ -17,3 +17,32 @@ export type AuthorizationResponse = {
     user: User;
     token: string;
 };
+
+type Sort = {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+};
+
+type Pageable = {
+    pageNumber: number;
+    pageSize: number;
+    sort: Sort;
+    offset: number;
+    unpaged: boolean;
+    paged: boolean;
+};
+
+export type Page<T> = {
+    content: T[];
+    pageable: Pageable;
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+    sort: Sort;
+    numberOfElements: 5;
+    first: boolean;
+    empty: boolean;
+};
