44841db12f13ec0fceac03a799e92c3bdc7ae798
Blindeen <kuba.szmajda@gmail.com>
2024-08-26 15:59:38 +0200

    add utlity functions

---
+++ src/utils/string.ts
@@ -0,0 +1,2 @@
+export const areStringsEqual = (string1: string, string2: string) =>
+    string1 === string2;
---
+++ src/utils/validators.ts
@@ -0,0 +1,11 @@
+import dayjs from 'dayjs';
+import { DateValue } from '@nextui-org/react';
+
+export const isUserAdult = (birthdate: DateValue) => {
+    const dateOfBirth = dayjs(birthdate.toString());
+    const yearsAgo = dayjs().subtract(18, 'years');
+    return (
+        dateOfBirth.isBefore(yearsAgo, 'days') ||
+        dateOfBirth.isSame(yearsAgo, 'days')
+    );
+};
