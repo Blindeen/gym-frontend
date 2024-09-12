170ba458b59e18ae3bc82f4aaabd20e7f6d25f6d
Blindeen <kuba.szmajda@gmail.com>
2024-09-12 11:59:18 +0200

    base dependency array on language instead of t func reference

--- src/components/PageRoute/index.tsx
+++ src/components/PageRoute/index.tsx
@@ -4,11 +4,14 @@ import { useTranslation } from 'react-i18next';
 import { PageRouteProps } from './types';
 
 const PageRoute = ({ children, tabTranslationCode }: PageRouteProps) => {
-    const { t } = useTranslation();
+    const {
+        t,
+        i18n: { language },
+    } = useTranslation();
 
     useEffect(() => {
         document.title = `FitSphere | ${t(`tabTitles.${tabTranslationCode}`)}`;
-    }, [t, tabTranslationCode]);
+    }, [language, tabTranslationCode]);
 
     return children;
 };
