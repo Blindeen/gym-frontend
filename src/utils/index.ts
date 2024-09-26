73d7824359ef1483aec5d99f983f1ce852fdd6bd
Blindeen <kuba.szmajda@gmail.com>
2024-09-26 10:47:16 +0200

    add utility function to convert base64 to src string

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,6 @@ export {
     removeLocalStorageItem,
 } from '@/utils/localStorage.ts';
 
-export { areStringsEqual } from '@/utils/string.ts';
+export { areStringsEqual, base64ToSrc } from '@/utils/string.ts';
 
 export { isUserAdult } from '@/utils/validators.ts';
--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,1 +1,3 @@
 export const areStringsEqual = (string1: string, string2: string) => string1 === string2;
+
+export const base64ToSrc = (base64: string, type: string) => `data:${type};base64,${base64}`;
