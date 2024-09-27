e300fcf06013fef3aec529ca611ad3d207903a99
Blindeen <kuba.szmajda@gmail.com>
2024-09-27 12:50:25 +0200

    add base64 to File converter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,6 @@ export {
     removeLocalStorageItem,
 } from '@/utils/localStorage.ts';
 
-export { areStringsEqual, base64ToSrc } from '@/utils/string.ts';
+export { areStringsEqual, base64ToSrc, base64ToFile } from '@/utils/string.ts';
 
 export { isUserAdult } from '@/utils/validators.ts';
--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,14 @@
 export const areStringsEqual = (string1: string, string2: string) => string1 === string2;
 
 export const base64ToSrc = (base64: string, type: string) => `data:${type};base64,${base64}`;
+
+export const base64ToFile = (name: string, type: string, base64: string) => {
+    const byteCharacters = atob(base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: type });
+    return new File([blob], name);
+};
