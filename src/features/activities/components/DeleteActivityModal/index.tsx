23a14a53efdae47b4a7c71e6c491f249e4b17e38
Blindeen <kuba.szmajda@gmail.com>
2024-10-25 12:36:12 +0200

    add delete activity modal

---
+++ src/features/activities/components/DeleteActivityModal/index.tsx
@@ -0,0 +1,56 @@
+import { Key } from 'react';
+
+import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+} from '@nextui-org/react';
+import { useTranslation } from 'react-i18next';
+import useRequest from '@hooks/useRequest';
+
+type DeleteActivityModalProps = {
+    activityId?: Key;
+    onClose?: () => void;
+    isOpen?: boolean;
+};
+
+const DeleteActivityModal = ({ activityId, onClose, isOpen }: DeleteActivityModalProps) => {
+    const { t } = useTranslation();
+
+    const { sendRequest, loadingRequest } = useRequest(`/activity/${activityId}/delete`, 'DELETE');
+
+    return (
+        <Modal placement="center" onClose={onClose} isOpen={isOpen}>
+            <ModalContent>
+                {(onClose) => (
+                    <>
+                        <ModalHeader className="flex flex-col gap-1">
+                            {t('deleteActivityModal.header')}
+                        </ModalHeader>
+                        <ModalBody>{t('deleteActivityModal.body')}</ModalBody>
+                        <ModalFooter>
+                            <Button color="default" onPress={onClose} isDisabled={loadingRequest}>
+                                {t('cancel')}
+                            </Button>
+                            <Button
+                                color="danger"
+                                onPress={async () => {
+                                    await sendRequest();
+                                    onClose();
+                                }}
+                                isLoading={loadingRequest}
+                            >
+                                {t('delete')}
+                            </Button>
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
+    );
+};
+
+export default DeleteActivityModal;
--- src/pages/DashboardPage/components/TrainerDashboard/index.tsx
+++ src/pages/DashboardPage/components/TrainerDashboard/index.tsx
@@ -1,13 +1,24 @@
+import { Key, useState } from 'react';
+
 import { useTranslation } from 'react-i18next';
+import { useDisclosure } from '@nextui-org/react';
 import { FaPen, FaPlus, FaTrash } from 'react-icons/fa6';
 
 import CustomTable from '@components/CustomTable';
 import { ActionButton } from '@components/CustomTable/types';
 
+import DeleteActivityModal from '@/features/activities/components/DeleteActivityModal';
 import { Activity } from '@/features/types';
 
 const TrainerDashboard = () => {
+    const [selectedActivityId, setSelectedActivityId] = useState<Key>();
+
     const { t } = useTranslation();
+    const {
+        isOpen: isDeleteModalOpen,
+        onOpen: onDeleteModalOpen,
+        onClose: onDeleteModalClose,
+    } = useDisclosure();
 
     const columns = [
         {
@@ -55,21 +66,29 @@ const TrainerDashboard = () => {
             children: t('delete'),
             startContent: <FaTrash />,
             color: 'danger',
-            onPress: () => console.log('deleted'),
+            onPress: () => onDeleteModalOpen(),
             alwaysEnabled: false,
         },
     ];
 
     return (
-        <div className="flex justify-center">
-            <div className="w-full md:w-3/4">
-                <CustomTable<Activity>
-                    columns={columns}
-                    url="/member/activities"
-                    actionButtons={buttons}
-                />
+        <>
+            <div className="flex justify-center">
+                <div className="w-full md:w-3/4">
+                    <CustomTable<Activity>
+                        columns={columns}
+                        url="/member/activities"
+                        actionButtons={buttons}
+                        onRowSelection={(rowId) => setSelectedActivityId(rowId)}
+                    />
+                </div>
             </div>
-        </div>
+            <DeleteActivityModal
+                activityId={selectedActivityId}
+                isOpen={isDeleteModalOpen}
+                onClose={onDeleteModalClose}
+            />
+        </>
     );
 };
 
